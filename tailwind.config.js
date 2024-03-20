05cf416b5c3e5142fc8652fbf7ffe0a7606b5110
DevelopWithHammad <hammadquiz@gmail.com>
2024-03-20 22:30:46 +0500

    twitter clone

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      maxWidth: {
+        "mywidth": "870px"
+      },
+      screens: {
+        'mymd': '962px'
+      }
+    },
+  },
+  plugins: [],
+}
